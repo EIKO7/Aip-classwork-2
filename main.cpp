0d6e2548e5f6ab64d194d3097b68d434f3efd8fe
EIKO7 <camper1773@mail.ru>
2025-11-24 12:58:18 +0300

    ver 2

--- main.cpp
+++ main.cpp
@@ -2,25 +2,41 @@
 
 struct Planar
 {
-	virtual int x() const;
-	virtual int y() const;
-	virtual int abc_sqr() const;
-	virtual ~Planar() = default;
+ virtual int x() const;
+ virtual int y() const;
+ virtual int abc_sqr() const;
+ virtual ~Planar() = default;
 };
 
 Planar* make(std::istream& is);
 Planar* most_left(Planar** pls, size_t k);
+Planar* make(size_t pl);
+void draw(Planar* pl);
+void free_planars(Planar** pls, size_t k);
 
 int main()
 {
-	Planar* pls[10] = {};
-	size_t k = 0;
+ Planar* pls[10] = {};
+ size_t k = 0;
 
-	//pls[k++] = make(std::cin);
+ //pls[k++] = make(std::cin);
 
-	Planar* left = most_left(pls, k);
+ Planar* left = most_left(pls, k);
 
-	//drow(left);
+ for (size_t i = 0; i < 10; ++i)
+ {
+  try
+  {
+   pls[k] = make(i % 2);
+  }
+  catch (...)
+  {
+   free_planars(pls, k);
+   return 2;
+  }
+  k++;
+ }
 
-	// free_planars(pls, k);
+ draw(most_left(pls,k));
+ free_planars(pls, k);
 }
